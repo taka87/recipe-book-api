89bf2e11c8afa1cebb06f40fb5ab2eda1c3871e3
Dani <daredevil@dir.bg>
2025-03-25 23:00:58 +0200

    Isolated Postgre SQL

--- Program.cs
+++ Program.cs
@@ -12,18 +12,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
 
 // 🔹 Провери дали използваме PostgreSQL (за Render)
-var usePostgreSQL = builder.Configuration.GetValue<bool>("UsePostgreSQL");
+//var usePostgreSQL = builder.Configuration.GetValue<bool>("UsePostgreSQL");
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") // URL на Angular приложението
-              .AllowAnyHeader()
+        policy.AllowAnyOrigin()
               .AllowAnyMethod()
-              .AllowCredentials(); // Ако използвате бисквитки/авторизация
+              .AllowAnyHeader();
     });
 });
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -40,15 +40,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 // 🔹 Конфигурирай базата данни
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    if (usePostgreSQL)
-    {
-        // PostgreSQL конфигурация за Render
-        var pgConnection = builder.Configuration.GetConnectionString("PostgreSQL")
-            ?? throw new InvalidOperationException("PostgreSQL connection string not found.");
-        options.UseNpgsql(pgConnection);
-    }
-    else
-    {
+    //if (usePostgreSQL)
+    //{
+    //    // PostgreSQL конфигурация за Render
+    //    var pgConnection = builder.Configuration.GetConnectionString("PostgreSQL")
+    //        ?? throw new InvalidOperationException("PostgreSQL connection string not found.");
+    //    options.UseNpgsql(pgConnection);
+    //}
+    //else
+    //{
         // MySQL конфигурация за локална разработка
         var mySqlConnection = builder.Configuration.GetConnectionString("MySQL")
             ?? throw new InvalidOperationException("MySQL connection string not found.");
@@ -56,45 +56,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
             mySqlConnection,
             new MySqlServerVersion(new Version(8, 0, 32))
         );
-    }
+    //}
 });
-
-
-// Works on Local Добави JWT Authentication
-// 🔹 Вземи connection string от appsettings.json
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-////Works on Render
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-// 🔹 Регистрирай DbContext-а с MySQL
-
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32))));
-
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAll", policy =>
-//    {
-//        Console.WriteLine("git test");
-//        policy.AllowAnyOrigin()
-//              .AllowAnyMethod()
-//              .AllowAnyHeader();
-//    });
-//});
-
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddJwtBearer(options =>
-//    {
-//        options.TokenValidationParameters = new TokenValidationParameters
-//        {
-//            ValidateIssuerSigningKey = true,
-//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ТВОЯТ_СУПЕР_СИГУРЕН_КЛЮЧ_12345")), // Замени с реален ключ
-//            ValidateIssuer = false,
-//            ValidateAudience = false
-//        };
-//    });
-
 
 builder.Services.AddControllers();
 
